28d4fd176817abc33670c58141ca2747e0a66f64
Sakib Ahmed <sakibahmad99090@gmail.com>
2021-03-21 19:21:47 +0600

    some changes adde

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,29 +1,35 @@
   export  const  data = [{
     name:"BIKE",
     imgURL:"https://www.nicepng.com/png/detail/364-3642406_bike-png-rc-200-vs-rc-125.png",
-    capacity:1
+    capacity:1,
+    price:500
 },{
     name:"CAR",
     imgURL:"https://www.pngitem.com/pimgs/m/638-6380605_toyota-corolla-car-png-transparent-png.png",
-    capacity:5
+    capacity:5,
+    price:1000
 },{
     name:"BUS",
     imgURL:"https://freepngimg.com/thumb/bus/6-2-bus-png-file-thumb.png",
-    capacity:50
+    capacity:50,
+    price:2500
 },{
     name:"TRAIN",
     imgURL:"http://pngimg.com/uploads/trian/trian_PNG101343.png",
-    capacity:1200
+    capacity:1200,
+    price:5000
 },{
     name:"HORS",
     imgURL:"https://pngimg.com/uploads/horse/horse_PNG2556.png",
-    capacity:1,
+    capacity:2000,
+    
 
 
 },{
-    name:"HORS",
+    name:"CYCLE",
     imgURL:"https://icon2.cleanpng.com/20180207/cyq/kisspng-bicycle-cycling-clip-art-bike-ride-png-file-5a7ac8f5ea0bf4.3982055415179962779587.jpg",
     capacity:1,
+    price:400
     
 
 }]
--- src/components/RiderPrice/RiderPrice.js
+++ src/components/RiderPrice/RiderPrice.js
@@ -1,43 +1,63 @@
-import React, { useContext } from 'react';
-import { LocationContext, RiderCategory } from '../../App';
-import {data} from "../Data/Data"
-import icon from "../../images/peopleicon.png"
-import Map from '../Map/Map';
-import "./RiderPrice.css"
+import React, { useContext } from "react";
+import { LocationContext, RiderCategory } from "../../App";
+import { data } from "../Data/Data";
+import icon from "../../images/peopleicon.png";
+import Map from "../Map/Map";
+import "./RiderPrice.css";
 
 const RiderPrice = () => {
-    const [rider,setRider] = useContext(RiderCategory)
-    const [location,setLocation] = useContext(LocationContext)
+  const [rider, setRider] = useContext(RiderCategory);
+  const [location, setLocation] = useContext(LocationContext);
 
-    const ridePrice = <>        <div className="mb-3 bg-white p-3 rounded">
-    <img style={{width:"60px"}} src={rider.imgURL ? rider.imgURL : data[0].imgURL} alt=""/> <p className=" d-inline ml-3 mr-2"> {rider.name? rider.name.toLowerCase():data[0].name.toLowerCase()}</p> <img style={{width:"20px"}} src={icon} alt=""/>
-       <strong> <p className="d-inline"> {rider.capacity?rider.capacity:data[0].capacity}</p></strong> <span className="ml-5"> $100</span>
-       </div>
+  const ridePrice = (
+    <>
+      {" "}
+      <div className="mb-3 bg-white p-3 rounded">
+        <img
+          style={{ width: "60px" }}
+          src={rider.imgURL ? rider.imgURL : data[0].imgURL}
+          alt=""
+        />{" "}
+        <p className=" d-inline ml-3 mr-2">
+          {" "}
+          {rider.name ? rider.name.toLowerCase() : data[0].name.toLowerCase()}
+        </p>{" "}
+        <img style={{ width: "20px" }} src={icon} alt="" />
+        <strong>
+          {" "}
+          <p className="d-inline">
+            {" "}
+            {rider.capacity ? rider.capacity : data[0].capacity}
+          </p>
+        </strong>{" "}
+        <span className="ml-5"> ${rider.price}</span>
+      </div>
     </>
-    console.log(rider);
-    return (
-        <div className="container d-flex flex-column flex-md-row pt-5 mt-2 border-top justify-content-center">
-       
-
-        <div className="rider-price">
-
+  );
+  console.log(rider);
+  return (
+    <div className="container d-flex flex-column flex-md-row pt-5 mt-2 border-top justify-content-center">
+      <div className="rider-price">
         <div className="fromToLocation p-3 text-white rounded ">
-        <h3><span style={{color:"red",marginRight:"8px"}}>From</span>{location.from}Dhaka</h3>
-        <h3 className="mt-3"><span style={{color:"red",marginRight:"8px"}}>To</span>{location.to}Rangpur</h3>
-
+          <h3>
+            <span style={{ color: "red", marginRight: "8px" }}>From</span>
+            {location.from}
+          </h3>
+          <h3 className="mt-3">
+            <span style={{ color: "red", marginRight: "8px" }}>To</span>
+            {location.to}
+          </h3>
         </div>
-           {ridePrice}
-            {ridePrice}
-            {ridePrice}
-            {ridePrice}
-
-        </div>
-        <div className="mt-md-0 mt-5">
-            <Map></Map>
-        </div>
-        </div>
-            
-    );
+        {ridePrice}
+        {ridePrice}
+        {ridePrice}
+        {ridePrice}
+      </div>
+      <div className="mt-md-0 mt-5">
+        <Map></Map>
+      </div>
+    </div>
+  );
 };
 
 export default RiderPrice;
